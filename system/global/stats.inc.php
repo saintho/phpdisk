bbd86c01bf68c660e4166d0f85630f20d0133d53
saintho <saintho123@gmail.com>
2015-09-22 02:10:11 +0000

    add lost file

--- system/global/stats.inc.php
+++ system/global/stats.inc.php
@@ -1,11 +1,11 @@
 <?php
 
 // This is PHPDISK auto-generated file. Do NOT modify me.
-// Cache Time: 2015-09-21 18:18:30
+// Cache Time: 2015-09-22 10:03:22
 
 $stats = array(
 	'all_files_count' => '0',
-	'stat_time' => '1442830709',
+	'stat_time' => '1442887402',
 	'total_storage_count' => '0.00 B',
 	'users_count' => '1',
 	'users_locked_count' => '0',
