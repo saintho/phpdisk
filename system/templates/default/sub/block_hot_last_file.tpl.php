44e81a8963542f62bf32399b0cf7e5f68dd1aceb
saintho <saintho123@gmail.com>
2015-09-22 01:45:48 +0000

    test

---
+++ system/templates/default/sub/block_hot_last_file.tpl.php
@@ -0,0 +1,53 @@
+<?php 
+// This is PHPDISK auto-generated file. Do NOT modify me.
+
+// Cache Time:2014-04-29 00:35:49
+
+!defined('IN_PHPDISK') && exit('[PHPDisk] Access Denied');
+
+?>
+<?php 
+##
+#	Project: PHPDISK File Storage Solution
+#	This is NOT a freeware, use is subject to license terms.
+#
+#	Site: http://www.google.com
+#
+#	$Id: block_hot_last_file.tpl.php 121 2014-03-04 12:38:05Z along $
+#
+#	Copyright (C) 2008-2014 PHPDisk Team. All Rights Reserved.
+#
+##
+ ?>
+<?php !defined('IN_PHPDISK') && exit('[PHPDisk] Access Denied!'); ?>
+<div class="fl_box">
+<div class="tit2"><?=__('last_file_list')?></div>
+<ul>
+<?php 
+if(count($C[last_file])){
+	foreach($C[last_file] as $v){
+ ?>
+	<li><?=$v['file_time']?><a href="<?=$v['a_viewfile']?>" target="_blank"><?=$v[file_icon]?> <?=$v['file_name']?></a></li>
+<?php 
+	}
+}
+ ?>
+</ul>
+</div>
+
+<div class="fl_box">
+<div class="tit2"><?=__('hot_file_list')?></div>
+<ul>
+<?php 
+if(count($C[hot_file])){
+	foreach($C[hot_file] as $v){
+ ?>
+	<li><?=$v['file_time']?><a href="<?=$v['a_viewfile']?>" target="_blank"><?=$v[file_icon]?> <?=$v['file_name']?></a></li>
+<?php 
+	}
+}
+ ?>
+</ul>
+</div>
+
+<div class="clear"></div>
---
+++ system/templates/default/sub/block_navigation_top.tpl.php
@@ -0,0 +1,31 @@
+<?php 
+// This is PHPDISK auto-generated file. Do NOT modify me.
+
+// Cache Time:2014-04-29 00:30:05
+
+!defined('IN_PHPDISK') && exit('[PHPDisk] Access Denied');
+
+?>
+<?php 
+##
+#	Project: PHPDISK File Storage Solution
+#	This is NOT a freeware, use is subject to license terms.
+#
+#	Site: http://www.google.com
+#
+#	$Id: block_navigation_top.tpl.php 121 2014-03-04 12:38:05Z along $
+#
+#	Copyright (C) 2008-2014 PHPDisk Team. All Rights Reserved.
+#
+##
+ ?>
+<?php !defined('IN_PHPDISK') && exit('[PHPDisk] Access Denied!'); ?>
+<?php 
+if(count($C[navi_top_link])){
+	foreach($C[navi_top_link] as $v){
+ ?>
+<a href="<?=$v['href']?>" target="<?=$v['target']?>" title="<?=$v['title']?>"><?=$v['text']?></a>&nbsp;
+<?php 
+	}
+}
+ ?>
