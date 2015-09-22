06e8113b0c27fd2997c6b678e278c4b651e89f06
saintho <saintho123@gmail.com>
2015-09-22 02:11:22 +0000

    add file

--- system/db_log.php
+++
@@ -1,8 +0,0 @@
-<?php exit(); ?>
-MySQL Info: 
-Error Code: 
-Query: Can not connect MySQL server!
-Time: 2015-09-21 18:22:43
-IP: 192.168.10.1
-phpdisk.app|/admincp.php?item=plugins&app=api
--------------------------
