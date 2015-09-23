3bbcddec1eecf99d789d6962379b17fd877770eb
saint <saintho123@gmail.com>
2015-09-23 16:00:59 +0800

    ffff

--- system/global/stats.inc.php
+++ system/global/stats.inc.php
@@ -1,11 +1,11 @@
 <?php
 
 // This is PHPDISK auto-generated file. Do NOT modify me.
-// Cache Time: 2015-09-23 15:04:49
+// Cache Time: 2015-09-23 15:38:22
 
 $stats = array(
 	'all_files_count' => '0',
-	'stat_time' => '1442983534',
+	'stat_time' => '1442993901',
 	'total_storage_count' => '0.00 B',
 	'users_count' => '1',
 	'users_locked_count' => '0',
